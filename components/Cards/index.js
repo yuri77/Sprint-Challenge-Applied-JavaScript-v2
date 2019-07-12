0ba21794456c66bbbace57bb0fbedfd3ba993e6a
yurika sakae <yurika.sun@gmail.com>
2019-07-12 11:38:09 -0700

    article bootstrap complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,14 +19,55 @@
 // Create a card for each of the articles and add the card to the DOM.
 
 const cardsContainer = document.querySelector(".cards-container");
+const articleTopics = [
+  "javascript",
+  "bootstrap",
+  "technology",
+  "jquery",
+  "node.js"
+];
 
 axios
   .get("https://lambda-times-backend.herokuapp.com/articles")
   .then(data => {
     console.log("card data is: ", data);
     const articles = data.data.articles;
-    console.log(articles);
+    console.log("articles in cards:", articles);
+    // articleTopics.forEach(aTopic => {
+    //   articles.aTopic.forEach(topic => console.log("topic: ", topic));
+    // });
+
+    articles.bootstrap.forEach(topic => {
+      console.log("bootstrap: ", topic);
+      cardsContainer.appendChild(createArticle(topic));
+    });
   })
   .catch(error => {
-    console.log("lambdaCard API is down: ", error);
+    console.log("The API is currently down: ", error);
   });
+
+function createArticle(article) {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const byAuthor = document.createElement("span");
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  headline.textContent = article.headline;
+  img.src = article.authorPhoto;
+  byAuthor.textContent = `By ${article.authorName}`;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(img);
+  author.appendChild(byAuthor);
+
+  return card;
+}

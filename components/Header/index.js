ac597ca69feb754b5d46e9f0b35b71a8f705b85b
yurika sakae <yurika.sun@gmail.com>
2019-07-12 09:34:01 -0700

    Header Components added

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,28 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(Header());
+
+function Header() {
+  const header = document.createElement("div");
+  const headerDate = document.createElement("span");
+  const title = document.createElement("h1");
+  const temp = document.createElement("span");
+
+  header.classList.add("header");
+  headerDate.classList.add("date");
+  temp.classList.add("temp");
+
+  headerDate.textContent = "March 28, 2019";
+  title.textContent = "Lambda Times";
+  temp.textContent = "98°";
+
+  header.appendChild(headerDate);
+  header.appendChild(title);
+  header.appendChild(temp);
+
+  return header;
+}
